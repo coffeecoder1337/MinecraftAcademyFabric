00661fbb87338e4544f1d0cde3a17e14824e178e
Vyacheslav Krokhalev <krox1337@yandex.ru>
2025-04-01 11:16:27 +0300

    init

---
+++ src/main/java/net/st1ch/minecraftacademy/MinecraftAcademy.java
@@ -0,0 +1,18 @@
+package net.st1ch.minecraftacademy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinecraftAcademy implements ModInitializer {
+	public static final String MOD_ID = "minecraft-academy";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/st1ch/minecraftacademy/MinecraftAcademyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.st1ch.minecraftacademy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinecraftAcademyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
