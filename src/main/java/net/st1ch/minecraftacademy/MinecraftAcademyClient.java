126148ba4d1fb3df715680272f13a4bc491d5896
Vyacheslav Krokhalev <krox1337@yandex.ru>
2025-04-01 11:31:03 +0300

    feat: client init

---
+++ src/main/java/net/st1ch/minecraftacademy/MinecraftAcademyClient.java
@@ -0,0 +1,10 @@
+package net.st1ch.minecraftacademy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftAcademyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
