a3234beb0edc6e73589df42d209c4a402a868a70
Vyacheslav Krokhalev <krox1337@yandex.ru>
2025-04-20 18:28:29 +0300

    style: remove comments, useless importys

--- src/main/java/net/st1ch/minecraftacademy/commands/user/GetTokenCommand.java
+++ src/main/java/net/st1ch/minecraftacademy/commands/user/GetTokenCommand.java
@@ -4,10 +4,6 @@ import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.ClickEvent;
-import net.minecraft.text.HoverEvent;
-import net.minecraft.text.Style;
-import net.minecraft.text.Text;
 import net.st1ch.minecraftacademy.auth.User;
 import net.st1ch.minecraftacademy.auth.UserManager;
 
--- src/main/java/net/st1ch/minecraftacademy/events/custom/PlayerJoinHandler.java
+++ src/main/java/net/st1ch/minecraftacademy/events/custom/PlayerJoinHandler.java
@@ -2,12 +2,8 @@ package net.st1ch.minecraftacademy.events.custom;
 
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.BlockPos;
 import net.st1ch.minecraftacademy.auth.User;
-import net.st1ch.minecraftacademy.entity.ModEntities;
-import net.st1ch.minecraftacademy.entity.custom.robot.RobotEntity;
 
 import static net.st1ch.minecraftacademy.MinecraftAcademy.userManager;
 
@@ -23,41 +19,5 @@ public class PlayerJoinHandler {
 
             player.sendMessage(Text.literal("Ваш токен: " + user.getToken()));
         });
-//        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-//            server.execute(() -> {
-//                ServerPlayerEntity player = handler.getPlayer();
-//                System.out.println("ID игрока: " + player.getId());
-//                ServerWorld world = player.getServerWorld();
-//
-//                // Устанавливаем точку спавна игрока
-//                BlockPos spawnPos = new BlockPos(100, 100, 100); // Координаты спавна
-//                player.teleport(world, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), 0, 0);
-//
-//                // Создаём мини-спавн (например, из блоков)
-//                buildMiniSpawn(world, spawnPos);
-//
-//                // Спавним робота рядом с игроком
-//                RobotEntity robot = new RobotEntity(ModEntities.ROBOT, world);
-//                robot.refreshPositionAndAngles(spawnPos.getX() + 1, spawnPos.getY(), spawnPos.getZ() + 1, 0, 0);
-//                String robotID = robot.getUuidAsString();
-//                world.spawnEntity(robot);
-////            UDPServer.registerRobot(robotID, robot);
-//
-//                // Отправляем сообщение в чат с ID робота
-//                player.sendMessage(Text.of("Ваш робот создан!" ), false);
-//                robot.sendRobotIdToChat(player, robotID);
-//            });
-//        });
-    }
-
-
-
-    private static void buildMiniSpawn(ServerWorld world, BlockPos pos) {
-        // Простая постройка вокруг точки спавна (например, из камня)
-        for (int x = -2; x <= 2; x++) {
-            for (int z = -2; z <= 2; z++) {
-                world.setBlockState(pos.add(x, -1, z), net.minecraft.block.Blocks.STONE.getDefaultState());
-            }
-        }
     }
 }
